33ccf267e988e94d846d4c5dffe1dfc5ef9c6eef
Saman <samangithoseinpour@gmail.com>
2023-12-25 11:29:41 +0330

    primitve data types

---
+++ data_types.py
@@ -0,0 +1,12 @@
+# Primitive Data Types
+age = 21
+print(type(age))
+
+name = 'saman'
+print(type(name))
+
+isLegal = True;
+print(type(isLegal))
+
+university_gpa = 18.38
+print(type(university_gpa))
