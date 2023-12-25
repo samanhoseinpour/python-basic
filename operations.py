1fbfa62628115b7cdc454c691129950c08be421d
samanhoseinpour <samangithoseinpour@gmail.com>
2023-12-25 13:17:31 +0330

    math operations

---
+++ operations.py
@@ -0,0 +1,26 @@
+# Math operations
+
+family_age = 21 + 25 + 52 + 59
+
+print(family_age)
+print(type(family_age))
+
+divide = 2 ** 3 // 2
+
+print(divide)
+print(type(divide))
+
+# calculate money amount after one year by 15% interest rate
+money_amount = 10000
+interest_rate = 0.15
+
+money_amount_next_year = (money_amount * interest_rate) + money_amount;
+
+print(money_amount_next_year)
+print(type(money_amount_next_year))
+
+# pow
+pow_calculate = 2 ** 8
+
+print(pow_calculate)
+print(type(pow_calculate))
