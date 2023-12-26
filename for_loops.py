ae3539b22698175ba400d878fd84886ce1582c24
samanhoseinpour <samangithoseinpour@gmail.com>
2023-12-26 10:05:49 +0330

    for loop

---
+++ for_loops.py
@@ -0,0 +1,15 @@
+start = 20
+end = -1
+step = -2
+
+for count in range(start, end, step):
+    print(count)
+
+print('----------------------------------------------')
+
+sum = 0 
+
+for number in range(sum, 10, 1): 
+    sum+=number
+
+print(sum)
