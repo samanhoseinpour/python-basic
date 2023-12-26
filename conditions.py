7bc15199313c2edb95d6753c0e3d81a68033cdee
samanhoseinpour <samangithoseinpour@gmail.com>
2023-12-26 09:09:01 +0330

    conditions

---
+++ conditions.py
@@ -0,0 +1,16 @@
+weather = 25
+
+if weather > 25 :
+    print('The weather is cold')
+else : 
+    print('weather is way too HOT')
+
+age_string = input("what's your age ? ")
+
+age = int(age_string)
+
+if(age > 22) : 
+    print('You can watch this movie')
+else: 
+    print('You CANNOT watch this movie')
+
